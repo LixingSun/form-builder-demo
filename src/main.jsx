d728197981105cc2b13f380346392a9de661d644
Lixing <slxtrjh325@gmail.com>
2024-06-14 18:00:03 +0800

    Init routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./routes/home";
+import Preview from "./routes/preview";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/preview",
+    element: <Preview />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/routes/preview.jsx
@@ -0,0 +1,5 @@
+export default function Preview() {
+  return (
+    <h1>Preview</h1>
+  );
+}
